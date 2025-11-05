fc188bba417afd77b24ba193c2c399b97944b908
Harshath Venkatesh <harshath.venkatesh@hestabit.in>
2025-11-05 16:54:30 +0530

    commit 7

--- app.js
+++ app.js
@@ -4,3 +4,4 @@ console.log(task on git advanced commands);
 console,log(bisect,revert,stash,cherry pick);
 console.log(currently doing task 1);
 console.log(creating 8 commits);
+console.log(with commit 4 as intentional bug);
